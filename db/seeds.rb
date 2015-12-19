874f066d94189cdccab716b9efb229f13d0c8bbc
Matthew Yang <yang70@gmail.com>
2015-12-19 07:49:59 -0800

    add rake task for random picks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,39 @@
-require 'csv'
+# require 'csv'
 
-CSV.foreach('lib/assets/bowls.csv') do |row|
+# CSV.foreach('lib/assets/bowls.csv') do |row|
 
-  game = Game.create!(  date_time: Time.parse(row[4]), 
-                        home_team: row[1], 
-                        away_team: row[2], 
-                        favorite: row[1], 
-                        underdog: row[2], 
-                        spread_for_away_team: row[3].to_f, 
-                        bowl_name: row[0],
-                        tv: row[5]
-                     )
-  game.save 
-end
+#   game = Game.create!(  date_time: Time.parse(row[4]), 
+#                         home_team: row[1], 
+#                         away_team: row[2], 
+#                         favorite: row[1], 
+#                         underdog: row[2], 
+#                         spread_for_away_team: row[3].to_f, 
+#                         bowl_name: row[0],
+#                         tv: row[5]
+#                      )
+#   game.save 
+# end
 
-emails = [
-  "hopejohanson@gmail.com",
-  "meivan@aol.com",
-  "yang70@gmail.com",
-  "xtinemueller@gmail.com",
-  "vny17@aol.com",
-  "muellermc@yahoo.com",
-  "mog110@hotmail.com",
-  "robyn.m.yang@gmail.com",
-  "fshn4x4@gmail.com",
-  "txyingyang@aol.com",
-  "kyle@freedomsolarpower.com",
-  "matt.walker921@gmail.com",
-  "frazierjen@gmail.com",
-  "melissa.Walker@solarwinds.com",
-  "txyingyang@gmail.com" 
-  ]
+# emails = [
+#   "hopejohanson@gmail.com",
+#   "meivan@aol.com",
+#   "yang70@gmail.com",
+#   "xtinemueller@gmail.com",
+#   "vny17@aol.com",
+#   "muellermc@yahoo.com",
+#   "mog110@hotmail.com",
+#   "robyn.m.yang@gmail.com",
+#   "fshn4x4@gmail.com",
+#   "txyingyang@aol.com",
+#   "kyle@freedomsolarpower.com",
+#   "matt.walker921@gmail.com",
+#   "frazierjen@gmail.com",
+#   "melissa.Walker@solarwinds.com",
+#   "txyingyang@gmail.com" 
+#   ]
 
-emails.each do |email|
-  User.create!(email: email, password: "changeme", password_confirmation: "changeme")
-end
+# emails.each do |email|
+#   User.create!(email: email, password: "changeme", password_confirmation: "changeme")
+# end
+#
+
---
+++ lib/tasks/random.rake
@@ -0,0 +1,32 @@
+desc "Random tasks to run"
+task :make_picks_mark_kyle => :environment do
+  mark = User.find(9)
+  kyle = User.find(11)
+
+  mark_picks = Pick.where(user: mark)
+  kyle_picks = Pick.where(user: kyle)
+
+  mark_picks.each do |pick|
+    game = Game.find(pick.game_id)
+    
+    if game.id.odd?
+      pick.winner = game.favorite
+    else
+      pick.winner = game.underdog
+    end
+
+    pick.save
+  end
+
+  kyle_picks.each do |pick|
+    game = Game.find(pick.game_id)
+
+    if game.id.even?
+      pick.winner = game.favorite
+    else
+      pick.winner = game.underdog
+    end
+
+    pick.save
+  end
+end
