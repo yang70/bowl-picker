0352d82b04db76b5ee76b46149ffd26691e05b86
Matthew Yang <yang70@gmail.com>
2015-12-22 19:49:30 -0800

    order games by date ascending

--- app/controllers/bowls_controller.rb
+++ app/controllers/bowls_controller.rb
@@ -2,7 +2,7 @@ class BowlsController < ApplicationController
   before_action :authenticate_user!
  
   def index
-    @games = Game.all
+    @games = Game.order(:date_time)
     @game_picks = {}
     @games.each do |game|
       @game_picks[game] = current_user.picks.find_by(game_id: game.id)
